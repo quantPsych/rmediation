f1c7f31d460fb80035700e65a54a03f1dd454aaf
quantpsych <dtofighi@gmail.com>
2024-03-08 23:14:08 +0000

    Mar 8, 2024, 4:14 PM

--- R/sem_mice.R
+++ R/sem_mice.R
@@ -4,23 +4,10 @@ sem_mice <- function(model, mids, ...) {
         stop("'mids' must be a 'mids' object from the 'mice' package.")
     }
     # Ensure 'mxModel' is an OpenMx model object
-    if (!inherits(model, "MxModel") ) {
-        stop("'mids' must be an 'MxModel' object from the 'OpenMx' package.")
-    } else {
-       mx_mice(model, mids, ...)
-    }
 
- if (!RMediation::is_valid_lav_syntax(model, mids$data)) {
-    stop("The model is not a valid lavaan model syntax.")
- } else {
-    lav_mice(model, mids, ...)
- }
-
-dplyr::case_when(
-    inherits(model, "MxModel") ~ mx_mice(model, mids, ...),
-    RMediation::is_valid_lav_syntax(model, mids$data) ~ lav_mice(model, mids, ...),
-    TRUE ~ stop("The model is not a valid lavaan or OpenMx model syntax.")
-)
-
+    dplyr::case_when(
+        inherits(model, "MxModel") ~ mx_mice(model, mids, ...),
+        RMediation::is_valid_lav_syntax(model, mids$data) ~ lav_mice(model, mids, ...),
+        TRUE ~ stop("The model is not a valid lavaan or OpenMx model syntax.")
+    )
 }
-

115404e0100d100d98e6f133d454521d62d4e697
quantpsych <dtofighi@gmail.com>
2024-03-08 23:50:00 +0000

    Mar 8, 2024, 4:50 PM

--- R/is_lav_syntax.R
+++ R/is_lav_syntax.R
@@ -17,13 +17,13 @@
 #' @examples
 #' bad_model <- "y ~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9"
 #' data(HolzingerSwineford1939, package = "lavaan")
-#' is_valid_lav_syntax(bad_model, HolzingerSwineford1939)
+#' is_lav_syntax(bad_model, HolzingerSwineford1939)
 #' good_model <- "visual =~ x1 + x2 + x3
 #' textual =~ x4 + x5 + x6
 #' speed =~ x7 + x8 + x9
 #' visual ~ speed
 #' textual ~ speed"
-#' is_valid_lav_syntax(good_model, HolzingerSwineford1939)
+#' is_lav_syntax(good_model, HolzingerSwineford1939)
 # Function to check if model syntax is valid
 is_lav_syntax <- function(model, data = NULL) {
   # Ensure data is a data frame and is not NULL

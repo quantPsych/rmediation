e6b110db38d050397bf3a2b93e32b63deb816bb2
quantpsych <dtofighi@gmail.com>
2024-03-19 20:09:09 +0000

    Mar 19, 2024, 2:09 PM

--- tests/testthat/test-SemImputedData.R
+++ tests/testthat/test-SemImputedData.R
@@ -89,7 +89,7 @@ test_that("set_sem executes correctly with OpenMx models", {
     )
   )
   # Check that the result is as expected
-  expect_no_error(sem_data <- set_sem(mx_model, data = imputed_data))
+  expect_no_error(sem_data <- set_sem(imputed_data, mx_model))
   expect_s4_class(sem_data, "SemImputedData")
   expect_equal(sem_data@method, "OpenMx")
   expect_equal(sem_data@n_imputations, 3)

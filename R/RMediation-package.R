f9ec87b5090f8f4d4d0194c15ee3dd1aa194a7df
quantpsych <dtofighi@gmail.com>
2024-03-21 03:37:44 +0000

    Mar 20, 2024, 9:37 PM

--- R/RMediation-package.R
+++ R/RMediation-package.R
@@ -1,12 +1,5 @@
 #' @keywords internal
 #' @importFrom utils globalVariables
-#' @import mice
-#' @importFrom lavaan sem lavInspect lavOptions
-#' @importFrom OpenMx mxModel mxRun mxData imxVerifyModel
-#' @importFrom purrr map_dfr map
-#' @importFrom methods setClass setValidity setMethod validObject setGeneric new
-#' @importFrom dplyr mutate select rename contains group_by summarise ungroup
-#' @importFrom tibble tibble as_tibble
 #' @importFrom broom tidy
 "_PACKAGE"
 
